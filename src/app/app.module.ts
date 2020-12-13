1c9aea07aecb5468eb65b26ad866a07e8efd6188
Ankita Thombare <anku1234thombare@gmail.com>
2020-12-13 14:53:12 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { ReadProductComponent } from './read-product/read-product.component';
+import { ShowProductsComponent } from './show-products/show-products.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateProductComponent,
+    ReadProductComponent,
+    ShowProductsComponent,
+    UpdateProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
